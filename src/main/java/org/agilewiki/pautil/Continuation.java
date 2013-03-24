a9191c918a2f26afa1bad9ac2ac63c9d447e5bb8
Bill La Forge <laforge49@gmail.com>
2013-03-24 15:32:19 +0530

    added Continuation

---
+++ src/main/java/org/agilewiki/pautil/Continuation.java
@@ -0,0 +1,40 @@
+package org.agilewiki.pautil;
+
+import org.agilewiki.pactor.Mailbox;
+import org.agilewiki.pactor.RequestBase;
+import org.agilewiki.pactor.ResponseProcessor;
+
+public class Continuation<RESPONSE_TYPE> implements ResponseProcessor<RESPONSE_TYPE> {
+    private Mailbox targetMailbox;
+    private ResponseProcessor<RESPONSE_TYPE> rp;
+
+    public Continuation(final Mailbox _targetMailbox, final ResponseProcessor _rp) {
+        targetMailbox = _targetMailbox;
+        rp = _rp;
+    }
+
+    @Override
+    public void processResponse(final RESPONSE_TYPE rsp) throws Exception {
+        new ContinuationRequest<RESPONSE_TYPE>(targetMailbox, rp, rsp);
+    }
+}
+
+class ContinuationRequest<RESPONSE_TYPE> extends RequestBase<Void> {
+    private ResponseProcessor<RESPONSE_TYPE> rp;
+    private RESPONSE_TYPE rsp;
+
+    public ContinuationRequest(final Mailbox targetMailbox,
+                               final ResponseProcessor<RESPONSE_TYPE> _rp,
+                               final RESPONSE_TYPE _rsp) {
+        super(targetMailbox);
+        rp = _rp;
+        rsp = _rsp;
+    }
+
+    @Override
+    public void processRequest(final ResponseProcessor<Void> _rp)
+            throws Exception {
+        rp.processResponse(rsp);
+        _rp.processResponse(null);
+    }
+}
