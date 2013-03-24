78b9346f8c5142e6b0aeb2ea344734c323dd4b0c
Bill La Forge <laforge49@gmail.com>
2013-03-24 13:55:39 +0530

    copied utilities from PActor

---
+++ src/main/java/org/agilewiki/pautil/Delay.java
@@ -0,0 +1,23 @@
+package org.agilewiki.pautil;
+
+import org.agilewiki.pactor.*;
+
+public class Delay {
+    private final Mailbox mailbox;
+
+    public Delay(final MailboxFactory mailboxFactory) {
+        this.mailbox = mailboxFactory.createMailbox();
+    }
+
+    public Request<Void> sleep(final long delay) {
+        return new RequestBase<Void>(mailbox) {
+            @Override
+            public void processRequest(
+                    final ResponseProcessor<Void> responseProcessor)
+                    throws Exception {
+                Thread.sleep(delay);
+                responseProcessor.processResponse(null);
+            }
+        };
+    }
+}
---
+++ src/main/java/org/agilewiki/pautil/ancestor/Ancestor.java
@@ -0,0 +1,7 @@
+package org.agilewiki.pautil.ancestor;
+
+import org.agilewiki.pactor.Actor;
+
+public interface Ancestor extends Actor {
+    public org.agilewiki.pactor.ancestor.Ancestor getParent();
+}
