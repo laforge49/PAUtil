7b98fb4a6125209b021b1dae55922fdf2dd60bd0
Bill La Forge <laforge49@gmail.com>
2013-03-25 15:30:42 +0530

    Atomic requests require autoflushing

--- src/main/java/org/agilewiki/pautil/AtomicProcessor.java
+++ src/main/java/org/agilewiki/pautil/AtomicProcessor.java
@@ -1,18 +1,28 @@
 package org.agilewiki.pautil;
 
-import org.agilewiki.pactor.ExceptionHandler;
-import org.agilewiki.pactor.Request;
-import org.agilewiki.pactor.RequestBase;
-import org.agilewiki.pactor.ResponseProcessor;
+import org.agilewiki.pactor.*;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class AtomicProcessor extends AncestorBase {
     private Queue<AtomicEntry> entries = new ArrayDeque<AtomicEntry>();
+    private Mailbox autoFlushMailbox;
+
+    @Override
+    public void initialize(final Mailbox _mailbox, final Ancestor _parent) {
+        super.initialize(_mailbox, _parent);
+        autoFlushMailbox = _mailbox.autoFlush();
+    }
+
+    @Override
+    public Mailbox getMailbox() {
+        return autoFlushMailbox;
+    }
+
 
     public Request<?> atomicReq(final Atomic _atom) {
-        return new RequestBase<Object>(getMailbox()) {
+        return new RequestBase<Object>(autoFlushMailbox) {
             @Override
             public void processRequest(final ResponseProcessor<Object> _rp) throws Exception {
                 entries.offer(new AtomicEntry(_atom, _rp));
@@ -22,18 +32,17 @@ public class AtomicProcessor extends AncestorBase {
     }
 
     private void process() throws Exception {
-        if (getMailbox().isEmpty() && !entries.isEmpty()) {
+        if (autoFlushMailbox.isEmpty() && !entries.isEmpty()) {
             final AtomicEntry entry = entries.remove();
             final Atomic atom = entry.atom;
             final ResponseProcessor<Object> _rp = new ResponseProcessor<Object>() {
                 @Override
                 public void processResponse(Object response) throws Exception {
                     entry.rp.processResponse(response);
-                    getMailbox().flush();
                     process();
                 }
             };
-            getMailbox().setExceptionHandler(new ExceptionHandler() {
+            autoFlushMailbox.setExceptionHandler(new ExceptionHandler() {
                 @Override
                 public void processException(Throwable throwable) throws Exception {
                     _rp.processResponse(throwable);
