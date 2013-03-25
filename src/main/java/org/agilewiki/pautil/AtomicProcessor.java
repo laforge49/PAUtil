977a69bf187c5cd48b6e7c77177738194934779e
Bill La Forge <laforge49@gmail.com>
2013-03-25 14:26:06 +0530

    Actor.mailbox is private, using getMailbox() now

--- src/main/java/org/agilewiki/pautil/AtomicProcessor.java
+++ src/main/java/org/agilewiki/pautil/AtomicProcessor.java
@@ -12,7 +12,7 @@ public class AtomicProcessor extends AncestorBase {
     private Queue<AtomicEntry> entries = new ArrayDeque<AtomicEntry>();
 
     public Request<?> atomicReq(final Atomic _atom) {
-        return new RequestBase<Object>(mailbox) {
+        return new RequestBase<Object>(getMailbox()) {
             @Override
             public void processRequest(final ResponseProcessor<Object> _rp) throws Exception {
                 entries.offer(new AtomicEntry(_atom, _rp));
@@ -22,18 +22,18 @@ public class AtomicProcessor extends AncestorBase {
     }
 
     private void process() throws Exception {
-        if (mailbox.isEmpty() && !entries.isEmpty()) {
+        if (getMailbox().isEmpty() && !entries.isEmpty()) {
             final AtomicEntry entry = entries.remove();
             final Atomic atom = entry.atom;
             final ResponseProcessor<Object> _rp = new ResponseProcessor<Object>() {
                 @Override
                 public void processResponse(Object response) throws Exception {
                     entry.rp.processResponse(response);
-                    mailbox.flush();
+                    getMailbox().flush();
                     process();
                 }
             };
-            mailbox.setExceptionHandler(new ExceptionHandler() {
+            getMailbox().setExceptionHandler(new ExceptionHandler() {
                 @Override
                 public void processException(Throwable throwable) throws Exception {
                     _rp.processResponse(throwable);
