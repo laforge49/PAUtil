ac462dcdb95c5c9c3a2c9655bf2959ebf1a55f3a
Bill La Forge <laforge49@gmail.com>
2013-03-25 14:20:21 +0530

    minor cleanup of Atomic

--- src/main/java/org/agilewiki/pautil/Atomic.java
+++ src/main/java/org/agilewiki/pautil/Atomic.java
@@ -3,5 +3,6 @@ package org.agilewiki.pautil;
 import org.agilewiki.pactor.ResponseProcessor;
 
 public interface Atomic<RESPONSE_TYPE> {
-    public void process(Ancestor processor, ResponseProcessor<RESPONSE_TYPE> rp) throws Exception;
+    public void process(final AtomicProcessor processor, final ResponseProcessor<RESPONSE_TYPE> rp)
+            throws Exception;
 }
