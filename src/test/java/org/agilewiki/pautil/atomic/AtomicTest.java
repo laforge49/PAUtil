ee5bc02265117ac0c820181faff97b6f1e294ecb
Bill La Forge <laforge49@gmail.com>
2013-03-26 12:01:19 +0530

    cleaned up AtomicTest

--- src/test/java/org/agilewiki/pautil/atomic/AtomicTest.java
+++ src/test/java/org/agilewiki/pautil/atomic/AtomicTest.java
@@ -10,19 +10,25 @@ public class AtomicTest extends TestCase {
     public void test1() throws Exception {
         MailboxFactory mailboxFactory = new DefaultMailboxFactoryImpl();
         try {
-            startReq1(mailboxFactory.createAsyncMailbox()).pend();
+            int count = startReq1(mailboxFactory.createAsyncMailbox()).pend();
+            assertEquals(5, count);
         } finally {
             mailboxFactory.close();
         }
     }
 
-    Request<Void> startReq1(final Mailbox _mailbox) {
-        return new RequestBase<Void>(_mailbox) {
+    Request<Integer> startReq1(final Mailbox _mailbox) {
+        return new RequestBase<Integer>(_mailbox) {
             @Override
-            public void processRequest(final ResponseProcessor<Void> _rp) throws Exception {
-                AtomicProcessorBase atomicProcessor = new FifoProcessor();
-                atomicProcessor.initialize(getMailbox().createAsyncMailbox());
-                ResponseProcessor rc = new ResponseCounter(5, _rp, null);
+            public void processRequest(final ResponseProcessor<Integer> _rp) throws Exception {
+                final AP ap = new AP();
+                ap.initialize(getMailbox().createAsyncMailbox());
+                ResponseProcessor rc = new ResponseCounter(5, new ResponseProcessor() {
+                    @Override
+                    public void processResponse(Object response) throws Exception {
+                        _rp.processResponse(ap.count);
+                    }
+                }, null);
                 AtomA atom1 = new AtomA();
                 AtomA atom2 = new AtomA();
                 AtomA atom3 = new AtomA();
@@ -33,14 +39,17 @@ public class AtomicTest extends TestCase {
                 atom3.msg = 3;
                 atom4.msg = 4;
                 atom5.msg = 5;
-                atomicProcessor.atomicReq(atom1).reply(_mailbox, rc);
-                atomicProcessor.atomicReq(atom2).reply(_mailbox, rc);
-                atomicProcessor.atomicReq(atom3).reply(_mailbox, rc);
-                atomicProcessor.atomicReq(atom4).reply(_mailbox, rc);
-                atomicProcessor.atomicReq(atom5).reply(_mailbox, rc);
+                ap.atomicReq(atom1).reply(_mailbox, rc);
+                ap.atomicReq(atom2).reply(_mailbox, rc);
+                ap.atomicReq(atom3).reply(_mailbox, rc);
+                ap.atomicReq(atom4).reply(_mailbox, rc);
+                ap.atomicReq(atom5).reply(_mailbox, rc);
             }
         };
     }
+}
+class AP extends FifoProcessor {
+    int count = 0;
 }
 
 class AtomA implements Atomic<Void> {
@@ -49,12 +58,15 @@ class AtomA implements Atomic<Void> {
     @Override
     public void process(final AtomicProcessorBase _processor, final ResponseProcessor<Void> _rp)
             throws Exception {
-        Mailbox mailbox = _processor.getMailbox();
+        final AP ap = (AP) _processor;
+        Mailbox mailbox = ap.getMailbox();
         Delay delay = new Delay(mailbox.getMailboxFactory());
         delay.sleepReq(100 - (msg * 20)).reply(mailbox, new ResponseProcessor<Void>() {
             @Override
             public void processResponse(Void response) throws Exception {
-                System.out.println(msg);
+                if (ap.count != msg -1)
+                    throw new IllegalStateException();
+                ap.count = msg;
                 _rp.processResponse(null);
             }
         });
