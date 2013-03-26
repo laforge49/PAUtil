e769a0e929cc561f298fd9e36b242dcc76d43e66
Bill La Forge <laforge49@gmail.com>
2013-03-26 10:52:03 +0530

    finished AtomicTest

--- src/test/java/org/agilewiki/pautil/atomic/AtomicTest.java
+++ src/test/java/org/agilewiki/pautil/atomic/AtomicTest.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 import org.agilewiki.pactor.*;
 import org.agilewiki.pamailbox.DefaultMailboxFactoryImpl;
 import org.agilewiki.pautil.Delay;
+import org.agilewiki.pautil.ResponseCounter;
 
 public class AtomicTest extends TestCase {
     public void test1() throws Exception {
@@ -21,6 +22,7 @@ public class AtomicTest extends TestCase {
             public void processRequest(final ResponseProcessor<Void> _rp) throws Exception {
                 AtomicProcessorBase atomicProcessor = new FifoProcessor();
                 atomicProcessor.initialize(getMailbox().createAsyncMailbox());
+                ResponseProcessor rc = new ResponseCounter(5, _rp, null);
                 AtomA atom1 = new AtomA();
                 AtomA atom2 = new AtomA();
                 AtomA atom3 = new AtomA();
@@ -31,56 +33,29 @@ public class AtomicTest extends TestCase {
                 atom3.msg = 3;
                 atom4.msg = 4;
                 atom5.msg = 5;
-                atomicProcessor.atomicReq(atom1).reply(_mailbox, new ResponseProcessor() {
-                    @Override
-                    public void processResponse(Object response) throws Exception {
-                        System.out.println(response);
-                        _rp.processResponse(null);
-                    }
-                });
-                /*
-                atomicProcessor.atomicReq(atom2).reply(_mailbox, new ResponseProcessor() {
-                    @Override
-                    public void processResponse(Object response) throws Exception {
-                        System.out.println(response);
-                    }
-                });
-                atomicProcessor.atomicReq(atom3).reply(_mailbox, new ResponseProcessor() {
-                    @Override
-                    public void processResponse(Object response) throws Exception {
-                        System.out.println(response);
-                    }
-                });
-                atomicProcessor.atomicReq(atom4).reply(_mailbox, new ResponseProcessor() {
-                    @Override
-                    public void processResponse(Object response) throws Exception {
-                        System.out.println(response);
-                    }
-                });
-                atomicProcessor.atomicReq(atom5).reply(_mailbox, new ResponseProcessor() {
-                    @Override
-                    public void processResponse(Object response) throws Exception {
-                        System.out.println(response);
-                    }
-                });
-                */
+                atomicProcessor.atomicReq(atom1).reply(_mailbox, rc);
+                atomicProcessor.atomicReq(atom2).reply(_mailbox, rc);
+                atomicProcessor.atomicReq(atom3).reply(_mailbox, rc);
+                atomicProcessor.atomicReq(atom4).reply(_mailbox, rc);
+                atomicProcessor.atomicReq(atom5).reply(_mailbox, rc);
             }
         };
     }
 }
 
-class AtomA implements Atomic<Integer> {
+class AtomA implements Atomic<Void> {
     public int msg;
 
     @Override
-    public void process(final AtomicProcessorBase _processor, final ResponseProcessor<Integer> _rp)
+    public void process(final AtomicProcessorBase _processor, final ResponseProcessor<Void> _rp)
             throws Exception {
         Mailbox mailbox = _processor.getMailbox();
         Delay delay = new Delay(mailbox.getMailboxFactory());
         delay.sleepReq(100 - (msg * 20)).reply(mailbox, new ResponseProcessor<Void>() {
             @Override
             public void processResponse(Void response) throws Exception {
-                _rp.processResponse(msg);
+                System.out.println(msg);
+                _rp.processResponse(null);
             }
         });
     }
