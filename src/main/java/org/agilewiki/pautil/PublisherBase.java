eb0092a02cecf0f436dd7df491c528877f67c855
skunkiferous <s.diot@eurodata.de>
2013-04-01 01:12:04 +0200

    Tried to optimize loop

--- src/main/java/org/agilewiki/pautil/PublisherBase.java
+++ src/main/java/org/agilewiki/pautil/PublisherBase.java
@@ -1,20 +1,25 @@
 package org.agilewiki.pautil;
 
-import org.agilewiki.pactor.*;
-
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
-public class PublisherBase<TARGET_ACTOR_TYPE extends Actor>
-        extends AncestorBase implements Publisher<TARGET_ACTOR_TYPE> {
-    private Set<TARGET_ACTOR_TYPE> subscribers = new HashSet<TARGET_ACTOR_TYPE>();
+import org.agilewiki.pactor.Actor;
+import org.agilewiki.pactor.ExceptionHandler;
+import org.agilewiki.pactor.Request;
+import org.agilewiki.pactor.RequestBase;
+import org.agilewiki.pactor.ResponseProcessor;
+import org.agilewiki.pactor.UnboundRequest;
+
+public class PublisherBase<TARGET_ACTOR_TYPE extends Actor> extends
+        AncestorBase implements Publisher<TARGET_ACTOR_TYPE> {
+    private final Set<TARGET_ACTOR_TYPE> subscribers = new HashSet<TARGET_ACTOR_TYPE>();
 
     @Override
-    public Request<Boolean> subscribeReq(final TARGET_ACTOR_TYPE _subscriber){
+    public Request<Boolean> subscribeReq(final TARGET_ACTOR_TYPE _subscriber) {
         return new RequestBase<Boolean>(getMailbox()) {
             @Override
-            public void processRequest(final ResponseProcessor<Boolean> _rp) throws Exception {
+            public void processRequest(final ResponseProcessor<Boolean> _rp)
+                    throws Exception {
                 _rp.processResponse(subscribers.add(_subscriber));
             }
         };
@@ -24,28 +29,33 @@ public class PublisherBase<TARGET_ACTOR_TYPE extends Actor>
     public Request<Boolean> unsubscribeReq(final TARGET_ACTOR_TYPE _subscriber) {
         return new RequestBase<Boolean>(getMailbox()) {
             @Override
-            public void processRequest(final ResponseProcessor<Boolean> _rp) throws Exception {
+            public void processRequest(final ResponseProcessor<Boolean> _rp)
+                    throws Exception {
                 _rp.processResponse(subscribers.remove(_subscriber));
             }
         };
     }
 
     @Override
-    public Request<Void> publishReq(final UnboundRequest<Void, TARGET_ACTOR_TYPE> event) {
+    public Request<Void> publishReq(
+            final UnboundRequest<Void, TARGET_ACTOR_TYPE> event) {
         return new RequestBase<Void>(getMailbox()) {
             @Override
-            public void processRequest(final ResponseProcessor<Void> _rp) throws Exception {
-                Set<TARGET_ACTOR_TYPE> subs = new HashSet<TARGET_ACTOR_TYPE>(subscribers);
-                Iterator<TARGET_ACTOR_TYPE> it = subs.iterator();
-                final ResponseCounter<Void> rc = new ResponseCounter<Void>(subs.size(), null, _rp);
+            public void processRequest(final ResponseProcessor<Void> _rp)
+                    throws Exception {
+                final Object[] subs = subscribers.toArray();
+                final ResponseCounter<Void> rc = new ResponseCounter<Void>(
+                        subs.length, null, _rp);
                 getMailbox().setExceptionHandler(new ExceptionHandler() {
                     @Override
-                    public void processException(Throwable throwable) throws Exception {
+                    public void processException(final Throwable throwable)
+                            throws Exception {
                         rc.decrementCount();
                     }
                 });
-                while (it.hasNext()) {
-                    TARGET_ACTOR_TYPE subscriber = it.next();
+                for (final Object object : subs) {
+                    @SuppressWarnings("unchecked")
+                    final TARGET_ACTOR_TYPE subscriber = (TARGET_ACTOR_TYPE) object;
                     event.send(getMailbox(), subscriber, rc);
                 }
             }
