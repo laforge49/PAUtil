d72d5d943778029e1f428ba7fa5f14f45923d8ea
Bill La Forge <laforge49@gmail.com>
2013-04-13 17:48:17 +0530

    javadocs for Ancestor and AncestorBase

--- src/main/java/org/agilewiki/pautil/Ancestor.java
+++ src/main/java/org/agilewiki/pautil/Ancestor.java
@@ -2,6 +2,13 @@ package org.agilewiki.pautil;
 
 import org.agilewiki.pactor.Actor;
 
+/**
+ * The Ancestor interface supports injection of an immutable dependency stack.
+ */
 public interface Ancestor extends Actor {
+    /**
+     * Returns the parent actor in the dependency stack.
+     * @return The parent actor, or null.
+     */
     public Ancestor getParent();
 }
--- src/main/java/org/agilewiki/pautil/AncestorBase.java
+++ src/main/java/org/agilewiki/pautil/AncestorBase.java
@@ -3,13 +3,30 @@ package org.agilewiki.pautil;
 import org.agilewiki.pactor.ActorBase;
 import org.agilewiki.pactor.Mailbox;
 
+/**
+ * Implements immutable dependency stack injection.
+ */
 public class AncestorBase extends ActorBase implements Ancestor {
+    /**
+     * Returns an ancestor, excluding the child, which is an instance of the target class.
+     *
+     * @param child       The actor who's dependency stack is searched.
+     * @param targetClass The class used to select the ancestor.
+     * @return The ancestor that is an instance of the target class, or null.
+     */
     public static Ancestor getAncestor(final Ancestor child, final Class targetClass) {
         if (child == null)
             return null;
         return getMatch(child.getParent(), targetClass);
     }
 
+    /**
+     * Returns the child, or an ancestor, which is an instance of the target class.
+     *
+     * @param child       The actor who's dependency stack is searched.
+     * @param targetClass The class used to select the child or one of its ancestors.
+     * @return An actor that implements the target class, or null.
+     */
     public static Ancestor getMatch(Ancestor child, final Class targetClass) {
         while (child != null) {
             if (targetClass.isInstance(child))
@@ -19,16 +36,33 @@ public class AncestorBase extends ActorBase implements Ancestor {
         return null;
     }
 
+    /**
+     * The top of the immutable dependency stack.
+     */
     private Ancestor parent;
 
+    /**
+     * Initialize the actor with an ancestor stack, but no mailbox.
+     *
+     * @param _parent The top of the immutable dependency stack.
+     */
     final public void initialize(final Ancestor _parent) throws Exception {
         initialize(null, _parent);
     }
 
+    /**
+     * Initialize the actor with a mailbox, but no dependency stack.
+     * @param _mailbox The mailbox used by the actor for message passing.
+     */
     final public void initialize(final Mailbox _mailbox) throws Exception {
         initialize(_mailbox, null);
     }
 
+    /**
+     * Initialize the actor with both a mailbox and an immutable dependency stack.
+     * @param _mailbox The mailbox used by the actor for message passing.
+     * @param _parent The top of the immutable dependency stack.
+     */
     public void initialize(final Mailbox _mailbox, final Ancestor _parent) throws Exception {
         super.initialize(_mailbox);
         parent = _parent;
