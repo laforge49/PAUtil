f9bf8cc13b0ce8802b604339149ada38595d7e78
Bill La Forge <laforge49@gmail.com>
2013-04-14 10:55:38 +0530

    Removed outdated comments from the PAIterator javadocs

--- src/main/java/org/agilewiki/pautil/PAIterator.java
+++ src/main/java/org/agilewiki/pautil/PAIterator.java
@@ -3,86 +3,14 @@ package org.agilewiki.pautil;
 import org.agilewiki.pactor.ResponseProcessor;
 
 /**
- * <p>
- * Iterates over its process method and works within any actor which supports 2-way messages.
- * </p>
- * <p>
- * We can use JAIterator to calculate factorials:
- * </p>
- * <pre>
- *        final int max = 5;
- *        RP printResult = new RP() {
- *            public void process(Object rsp) {
- *                System.out.println(rsp);
- *            }
- *        };
- *
- *        (new JAIterator() {
- *            int i;
- *            int r = 1;
- *
- *            public void process(RP rp) throws Exception {
- *                if (i >= max) rp.process(new Integer(r));
- *                else {
- *                    i += 1;
- *                    r = r * i;
- *                    rp.process(null);
- *                }
- *            }
- *        }).iterate(printResult);
- * </pre>
- * <p>
  * The JAIterator.process method is called repeatedly until it returns a non-null response,
- * which is then returned by JAIterator. However, the response need not be returned immediately, so the
- * JAIterator.process method can send messages to other actors:
- * </p>
- * <pre>
- * public class Factorial extends JLPCActor {
- *
- *     public Factorial(Mailbox mailbox) {
- *         super(mailbox);
- *     }
- *
- *     protected void processRequest(Object req, final RP rp)
- *             throws Exception {
- *         final int max = 5;
- *         RP printResult = new RP() {
- *             public void process(Object rsp) throws Exception {
- *                 System.out.println(rsp);
- *                 rp.process(null);
- *             }
- *         };
- *         (new JAIterator() {
- *             int i;
- *             int r = 1;
- *             Multiplier mp = new Multiplier(getMailbox());
- *
- *             protected void process(RP rp) throws Exception {
- *                 if (i >= max) rp.process(new Integer(r));
- *                 else {
- *                     i += 1;
- *                     Multiply m = new Multiply();
- *                     m.a = r;
- *                     m.b = i;
- *                     send(mp, m, new RP() {
- *                         public void process(Object rsp) throws Exception {
- *                             r = ((Integer) rsp).intValue();
- *                         }
- *                     });
- *                     rp.process(null);
- *                 }
- *             }
- *         }).iterate(printResult);
- *     }
- * }
- * </pre>
+ * which is then returned by JAIterator.
  */
 abstract public class PAIterator {
     /**
-     * Iterates over the process method.
+     * Iterates over the process method until there is a non-null result.
      *
      * @param responseProcessor The response processor.
-     * @throws Exception Any uncaught exceptions raised by the process method.
      */
     public void iterate(final ResponseProcessor responseProcessor)
             throws Exception {
@@ -103,7 +31,6 @@ abstract public class PAIterator {
      * Perform an iteration.
      *
      * @param responseProcessor Processes the response.
-     * @throws Exception Any uncaught exceptions raised by the process method.
      */
     abstract protected void process(final ResponseProcessor responseProcessor)
             throws Exception;
