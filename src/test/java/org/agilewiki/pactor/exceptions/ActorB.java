6db5c03e9c837999577f6fe8790ab0c8cefbd6dc
Bill La Forge <laforge49@gmail.com>
2013-04-22 12:54:57 +0530

    moved tests from pamailbox

---
+++ src/test/java/org/agilewiki/pactor/exceptions/ActorB.java
@@ -0,0 +1,25 @@
+package org.agilewiki.pactor.exceptions;
+
+import org.agilewiki.pactor.Mailbox;
+import org.agilewiki.pactor.Request;
+import org.agilewiki.pactor.RequestBase;
+import org.agilewiki.pactor.ResponseProcessor;
+
+public class ActorB {
+    private final Mailbox mailbox;
+
+    public ActorB(final Mailbox mbox) {
+        this.mailbox = mbox;
+    }
+
+    public Request<Void> throwRequest(final ActorA actorA) {
+        return new RequestBase<Void>(mailbox) {
+            @Override
+            public void processRequest(
+                    final ResponseProcessor<Void> responseProcessor)
+                    throws Exception {
+                actorA.throwRequest.send(mailbox, responseProcessor);
+            }
+        };
+    }
+}
---
+++ src/test/java/org/agilewiki/pactor/messaging/Actor4.java
@@ -0,0 +1,35 @@
+package org.agilewiki.pactor.messaging;
+
+import org.agilewiki.pactor.Mailbox;
+import org.agilewiki.pactor.Request;
+import org.agilewiki.pactor.RequestBase;
+import org.agilewiki.pactor.ResponseProcessor;
+
+/**
+ * Test code.
+ */
+public class Actor4 {
+    private final Mailbox mailbox;
+
+    public Actor4(final Mailbox mbox) {
+        this.mailbox = mbox;
+    }
+
+    public Request<Void> hi4(final Actor1 actor1) {
+        return new RequestBase<Void>(mailbox) {
+            @Override
+            public void processRequest(
+                    final ResponseProcessor<Void> responseProcessor)
+                    throws Exception {
+                actor1.hi1.send(mailbox, new ResponseProcessor<String>() {
+                    @Override
+                    public void processResponse(final String response)
+                            throws Exception {
+                        System.out.println(response);
+                        responseProcessor.processResponse(null);
+                    }
+                });
+            }
+        };
+    }
+}
